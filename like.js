3250a3883e1129806e7bc1e5c57152010d9616e1
Unknown <jdeansart@gmail.com>
2017-10-02 13:08:42 -0500

    1st push

--- like.js
+++ like.js
@@ -1,8 +1,26 @@
-var clicks = document.getElementById();
-function click() {
-    clicks += 1;
-    document.getElementById("likes").innerHTML = clicks;
+
+function onClick() {
+    let clicks = document.getElementById('likes').nextSibling
+    let likes = parseInt(clicks.textContent) + 1;
+    clicks.textContent = likes;
+
+
 };
 let buttons = document.getElementById("likebutton");
 
-buttons.addEventListener("click", click);
+buttons.addEventListener("click", onClick);
+
+
+
+
+
+// get elements by class name, and convert to an array using Array.from()
+// Foreach element in the array,
+// attach an event listener for a "click" event
+
+// When the user clicks on a configured button,
+// Find the current count *next to* the clicked button
+// Increment it
+// Set it back
+
+// #tableflip
